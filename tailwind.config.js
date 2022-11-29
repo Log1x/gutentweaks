577c7e799dd99062846f76f520379876a4ace5e2
Brandon Nifong <brandon@tendency.me>
2022-11-29 06:04:22 -0600

    chore(deps): Remove unnecessary Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  purge: false,
-  theme: {
-    extend: {
-      fontFamily: {
-        fa: ['"Font Awesome 5 Pro"', '"Font Awesome 5 Free"'],
-      },
-    },
-  },
   variants: {},
   plugins: [],
 }
